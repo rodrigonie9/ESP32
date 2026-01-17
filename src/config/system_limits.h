b7547a3cafe91c61e04efb8e65f3f5e1584dbaf7
PCdeposito <rodrigonie9@gmail.com>
2026-01-17 19:54:47 -0300

    logica alerta

--- src/config/system_limits.h
+++ src/config/system_limits.h
@@ -1,2 +1,9 @@
 #pragma once // Limite máximo de sensores DS18B20 por placa 
 #define MAX_SENSORES 30
+
+// tempo de espera para temperatura crítica
+#define TEMPO_SEGURANCA_CRITICO_MS 180000  // 3 minutos em milissegundos
+
+// Intervalos de tempo (em milissegundos)
+#define INTERVALO_LEITURA_SENSORES_MS 120000 // 2 minutos  leitura dos sensores
+#define INTERVALO_RELATORIO_ROTINA_MS 900000 // 15 minutos 
--- src/logic/alert_manager.cpp
+++ src/logic/alert_manager.cpp
@@ -4,6 +4,7 @@
 #include "../telegram/telegram.h"
 #include "../debug.h"
 #include "../config/system_limits.h"
+
 // Estrutura interna para controlar o tempo de cadas sensor sem usar a NVS
 struct EstadoAlerta{
     unsigned long inicioAlertaSuave = 0;
@@ -23,6 +24,17 @@ void processarLogicaAlertas() {
         // Verifica se está no modo manutençao (mudo)
         if (s.mudo_ate > 0 && millis() < s.mudo_ate) continue;
 
+        float temp = getTemperaturaSensorPorID(s.id_fisico);
+        if (temp == SENSOR_ERRO) continue;
 
+        uint8_t idx = getSensorIndicePorId(s.id_fisico);
+        if (idx == 255) continue;
+
+        // --- LÓGICA 1: ALERTA CRÍTICO (Imediato / Rápido) ---
+        if (temp >= s.temp_critica {
+            if (estados[idx].inicioAlertaCritico == 0) {   //não inciou contagem
+                estados[idx].inicioAlertaCritico = millis();
+            } else if 
+        })
     }
 }
